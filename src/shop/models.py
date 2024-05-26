6d710a5d6cd10a40a14f05494ac81ddeb376476d
vladislav_finogenov <Finogenov.V.A@yandex.ru>
2024-05-26 13:06:24 +0300

    Adding Models

--- src/shop/models.py
+++ src/shop/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.urls import reverse
-from taggit.managers import TaggableManager
 from django.conf import settings
+from django.contrib.auth import get_user_model
+from taggit.managers import TaggableManager
 
 
 def product_preview_directory_path(instance: "Product", filename: str) -> str:
@@ -11,10 +12,44 @@ def product_preview_directory_path(instance: "Product", filename: str) -> str:
     )
 
 
+def seller_thumbnail_directory_path(instance: "Seller", filename: str) -> str:
+    return f"shop/seller_thumbnails/seller_{instance.pk}/{filename}"
+
+
+class HistoryProductManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().select_related('product__category__parent')
+
+
+class HistoryProduct(models.Model):
+    """
+    Модель HistoryProduct представляет
+    историю пользователя просмотра продуктов.
+    """
+
+    class Meta:
+        ordering = ['-created_at']
+
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    product = models.ForeignKey(
+        to='Product',
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    objects = models.Manager()
+    history = HistoryProductManager()
+
+
 class Category(models.Model):
     """
-      Модель Category представляет категорию
-      для модели Product, указанная ниже.
+    Модель Category представляет категорию
+    для модели Product, указанная ниже.
     """
 
     class Meta:
@@ -22,9 +57,21 @@ class Category(models.Model):
         verbose_name = "category"
         verbose_name_plural = 'Categories'
 
-    name = models.CharField(max_length=100, db_index=True)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
-    icon = models.ImageField(upload_to='category_icons/', blank=True, null=True)
+    name = models.CharField(
+        max_length=100,
+        db_index=True
+    )
+    parent = models.ForeignKey(
+        to='self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    icon = models.ImageField(
+        upload_to='category_icons/',
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
         return self.name
@@ -32,8 +79,8 @@ class Category(models.Model):
 
 class Product(models.Model):
     """
-      Модель Product представляет товар,
-      кoоторый можно продавать в интернет-магазине.
+    Модель Product представляет товар,
+    который можно продавать в интернет-магазине.
     """
 
     class Meta:
@@ -41,10 +88,24 @@ class Product(models.Model):
         verbose_name = "product"
         verbose_name_plural = "products"
 
-    name = models.CharField(max_length=100, db_index=True)
-    description = models.TextField(blank=True, null=True)
-    preview = models.ImageField(null=True, blank=True, upload_to=product_preview_directory_path)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(
+        max_length=100,
+        db_index=True
+    )
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+    preview = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to=product_preview_directory_path
+    )
+    category = models.ForeignKey(
+        to=Category,
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
     available = models.BooleanField(default=True)
     tags = TaggableManager(blank=True)
 
@@ -52,7 +113,7 @@ class Product(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse("product_details", kwargs={"pk": self.pk})
+        return reverse("shop:product_detail", kwargs={"pk": self.pk})
 
 
 class Review(models.Model):
@@ -87,3 +148,108 @@ class Review(models.Model):
 
     def __str__(self):
         return f'Отзыв от {self.created_at}'
+
+
+class Attribute(models.Model):
+    """
+    Модель Attribute определяет структуру данных для хранения характеристик товаров.
+    Имеет связь один-ко-многим с Category.
+    Поля name (имя характеристики) и unit (единица измерения)
+    """
+
+    name = models.CharField(
+        max_length=100,
+        verbose_name='характеристика'
+    )
+    unit = models.CharField(
+        max_length=50,
+        blank=True,
+        default='',
+        verbose_name='единица измерения'
+    )
+    category = models.ForeignKey(
+        to=Category,
+        blank=True,
+        null=True,
+        related_name='attribute_names',
+        on_delete=models.CASCADE,
+        verbose_name='категория'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class ProductAttribute(models.Model):
+    """
+    Модель ProductAttribute связывает конкретный продукт (товар) с его характеристиками.
+    Также связь с моделью Attribute.
+    """
+
+    product = models.ForeignKey(
+        Product,
+        related_name='attributes',
+        on_delete=models.CASCADE,
+        verbose_name='товар'
+        )
+    attribute = models.ForeignKey(
+        Attribute,
+        related_name='attributes',
+        on_delete=models.CASCADE,
+        verbose_name='характеристика',
+      )
+    value = models.CharField(
+        max_length=250,
+        verbose_name='значение'
+    )
+
+
+class Seller(models.Model):
+    """
+    Модель Seller представляет продавца,
+    который может размещать свои товары (SellerProduct) в магазине.
+    """
+
+    class Meta:
+        ordering = ["name"]
+
+    user = models.OneToOneField(
+        to=settings.AUTH_USER_MODEL,
+        related_name="seller",
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(
+        max_length=20,
+        db_index=True,
+        null=False
+    )
+    thumbnail = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to=seller_thumbnail_directory_path
+    )
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    def get_absolute_url(self):
+        return reverse("seller_details", kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return self.name
+
+
+class SellerProduct(models.Model):
+    """
+    Модель SellerProduct представляет товар, который продает конкретный продавец (Seller).
+    Эта модель связана с Product и отличается ценой и количеством от продавца к продавцу.
+    """
+
+    seller = models.ForeignKey(Seller, related_name="products", on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, related_name="seller_products", on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.product.name
