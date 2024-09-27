cd9c6ba7afe1af499d9b7e920c633cc36caa76d7
vlad <Finogenov.V.A@yandex.ru>
2024-09-28 02:49:36 +0300

    feat: добавлена путь до оплаты

--- src/megano/urls.py
+++ src/megano/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('banners/', include('banners.urls')),
     path('orders/', include('orders.urls')),
-    path('payments/', include('payments.urls', namespace='payments')),
+    path('payments/', include('payments.urls')),
 ]
 
 urlpatterns += i18n_patterns(
--- src/payments/urls.py
+++ src/payments/urls.py
@@ -5,6 +5,7 @@ from .views import PaymentCanceled, PaymentProcess
 app_name = "payment"
 
 urlpatterns = [
-    path("payment-process/<int:id>/", PaymentProcess.as_view(), name="checkout"),
+    path('create/', PaymentProcess.as_view(), name='payment_create'),
+    # path("payment-process/<int:id>/", PaymentProcess.as_view(), name="checkout"),
     path("payment-canceled/", PaymentCanceled.as_view(), name="canceled"),
 ]
